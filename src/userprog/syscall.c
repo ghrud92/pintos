b36b58ffa6414841448267b0f94bddaff8f0e45d
hokyung <ghrud92@gmail.com>
2017-10-12 23:47:59 +0900

    디버깅 디버깅

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -27,7 +27,7 @@ bool create (const char * file, unsigned initial_size)
 int open (const char * file)
 {
   static int nextfd = 2;
-  struct openedfile *opfile = (openedfile*) malloc (sizeof(openedfile) * 1);
+  struct openedfile *opfile = malloc (sizeof(struct openedfile) * 1);
   if (file != NULL)
   {
     opfile -> file = filesys_open (*file);
@@ -54,8 +54,8 @@ void close (int fd)
         list_remove(&(now->opelem));
         return;
       }
-      now = list_entry(list_next(&(now->elem)), struct openedfile, opelem);
-    } while(now->elem.next != NULL);
+      now = list_entry(list_next(&(now->opelem)), struct openedfile, opelem);
+    } while(now->opelem.next != NULL);
   }
   return;
 }
@@ -67,16 +67,19 @@ void halt ()
 
 void exit (int status)
 {
-  struct openedfile * now = list_entry(list_front(&opfilelist), struct openedfile, opelem);
-  do
+  if(!list_empty(&opfilelist))
   {
-    if (now -> caller == thread_current())
+    struct openedfile * now = list_entry(list_front(&opfilelist), struct openedfile, opelem);
+    do
     {
-      file_close (now -> file);
-      list_remove(&(now->opelem));
-    }
-    now = list_entry(list_next(&(now->elem)), struct openedfile, opelem);
-  } while(now->elem.next != NULL);
+      if (now -> caller == thread_current())
+      {
+        file_close (now -> file);
+        list_remove(&(now->opelem));
+      }
+      now = list_entry(list_next(&(now->opelem)), struct openedfile, opelem);
+    } while(now->opelem.next != NULL);
+  }
   thread_current()->exit_status = status;
   thread_exit();
 }
@@ -98,22 +101,22 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_HALT:
       halt();
       break;
-    case SYS_CREATE
+    case SYS_CREATE:
       if(!is_user_vaddr(*(ptr+4)) || !is_user_vaddr(ptr+5))
         exit(-1);
       create(ptr+4, *(ptr+5));
       break;
-    case SYS_OPEN
+    case SYS_OPEN:
       if(!is_user_vaddr(*(ptr+1)))
         exit(-1);
       f -> eax = open (ptr+1);
       break;
-    case SYS_CLOSE
+    case SYS_CLOSE:
       if(!is_user_vaddr(ptr+1))
         exit(-1);
       close (*(ptr+1));
       break;
-    case SYS_EXIT
+    case SYS_EXIT:
       if(!is_user_vaddr(ptr+1))
         exit(-1);
       exit(*(ptr+1));
