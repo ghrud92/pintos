3bd3075413f84536bc5b61e0f20498319c4a4d3c
pki663 <novelmaterial@gmail.com>
2017-10-23 16:15:15 +0900

    thread 구조체에 bool 멤버 ready_to_exit 추가 및 process_wait 의 작동원리 변경

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,6 +93,7 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
     int exit_status;
+    bool ready_to_exit = false;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
--- src/userprog/process.c
+++ src/userprog/process.c
@@ -24,7 +24,6 @@
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
-static bool exit = false;
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
@@ -159,13 +158,11 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED)
 {
-  exit = false;
+  thread_current ()->ready_to_exit = false;
   while (true)
   {
-    if (exit)
-    {
+    if (thread_current ()->ready_to_exit)
       break;
-    }
   }
   return -1;
 }
@@ -176,7 +173,7 @@ process_exit (void)
 {
   struct thread *cur = thread_current ();
   uint32_t *pd;
-  exit = true;
+  cur->ready_to_exit = true;
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
