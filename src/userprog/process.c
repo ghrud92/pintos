de5403a1e37d6696ee6ffe348d5821d2a06075a1
hokyung <ghrud92@gmail.com>
2017-11-23 21:26:44 +0900

    get_page 구현 완료. 컴파일 에러 수정.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -425,7 +425,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
 
 /* load() helpers. */
 
-static bool install_page (void *upage, void *kpage, bool writable);
+// static bool install_page (void *upage, void *kpage, bool writable);
 
 /* Checks whether PHDR describes a valid, loadable segment in
    FILE and returns true if so, false otherwise. */
@@ -582,8 +582,8 @@ setup_stack (void **esp)
    with palloc_get_page().
    Returns true on success, false if UPAGE is already mapped or
    if memory allocation fails. */
-static bool
-install_page (void *upage, void *kpage, bool writable)
+// static bool
+bool install_page (void *upage, void *kpage, bool writable)
 {
   struct thread *t = thread_current ();
 
--- src/vm/page.c
+++ src/vm/page.c
@@ -1,5 +1,8 @@
 #include "vm/page.h"
 #include "vm/frame.h"
+#include "userprog/process.h"
+#include "threads/thread.h"
+#include "threads/vaddr.h"
 
 void init_page_table(struct list* page_table)
 {
@@ -19,19 +22,39 @@ bool load_page(struct page* page)
         return false;
 
     // load this page
-    if (file_read (page -> file, kpage, page -> page_read_bytes) != (int) page_raed_bytes)
+    if (file_read (page -> file, kpage, page -> read_bytes) != (int) page -> read_bytes)
     {
         free_frame(kpage);
         return false;
     }
-    memset (kpage + page -> page_read_bytes, 0, page -> page_zero_bytes);
+    memset (kpage + page -> read_bytes, 0, page -> zero_bytes);
 
     // add the page to the process's address space
-    if (!install_page (upage, kpage, writable))
+    if (!install_page (page -> upage, kpage, page -> writable))
     {
         free_frame (kpage);
         return false;
     }
 
     return true;
+}
+
+struct page* get_page(uint8_t* upage)
+{
+    struct page temp;
+    temp.upage = pg_round_down(upage);
+
+    struct list_elem* e;
+    struct thread* t = thread_current();
+    for (e = list_begin(&t -> page_table);
+        e != list_end(&t -> page_table);
+        e = list_next(&t -> page_table))
+    {
+        struct page* current = list_entry(e, struct page, elem);
+        if (current -> upage == temp.upage)
+        {
+            return current;
+        }
+    }
+    return NULL;
 }
